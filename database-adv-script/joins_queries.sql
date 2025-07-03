c6db4022888af209ce72e714ff973c9a603b77b9
Yared <alemayehuyared9@gmail.com>
2025-07-03 09:31:30 -0700

    add order by property id when retrieving property with review and without

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -20,6 +20,7 @@ SELECT
        r.comment
 FROM properties p
 LEFT JOIN reviews r ON p.property_id = r.property_id;
+ORDER BY p.property_id;
 
 
 -- Write a query using a FULL OUTER JOIN to retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user.
