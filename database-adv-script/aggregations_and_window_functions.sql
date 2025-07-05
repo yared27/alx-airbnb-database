1e58b495b29e3940eb6845570d5a269ddace8a4b
Yared <alemayehuyared9@gmail.com>
2025-07-05 08:28:27 -0700

    add row number for booking rank

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -9,7 +9,14 @@ GROUP BY user_id;
 
 SELECT 
      property_id,
-     COUNT(*) AS total_bookings
+     COUNT(*) AS total_bookings,
      RANK() OVER (ORDER BY COUNT(*) DESC) AS booking_rank
 FROM bookings
 GROUP BY property_id
+
+SELECT 
+    property_id,
+    COUNT(*) AS total_bookings,
+    ROW_NUMBER() OVER ( ORDER BY COUNT(*) DESC) AS booking_rank
+FROM bookings 
+GROUP BY property_id;
